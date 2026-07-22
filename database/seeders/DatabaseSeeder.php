143a910e05f960933a61724a91c13c723847991e
unknown <naboth879@gmail.com>
2026-07-22 16:52:11 +0300

    Register seeders in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,19 +6,14 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
-            // Add your seeder classes here, e.g.:
-            // UserSeeder::class,
-            // JobSeeder::class,
-            // ApplicationSeeder::class,
-            // ContactSeeder::class,
-            // MessageSeeder::class,
-            // PaymentSeeder::class,
+            JobPostingSeeder::class,
+            ApplicationSeeder::class,
+            ContactSeeder::class,
+            MessageSeeder::class,
+            PaymentSeeder::class,
         ]);
     }
 }
