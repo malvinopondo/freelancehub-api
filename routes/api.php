6f850d5af15d806d47050604b96ecde65225d370
Malvin Ochieng <79262820+malvinopondo@users.noreply.github.com>
2026-08-01 20:57:48 +0300

    Add authentication routes for login and logout

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
 
 Route::get('/jobs',         [ApiController::class, 'jobs']);
 Route::get('/applications', [ApiController::class, 'applications']);
