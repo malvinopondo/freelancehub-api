6c991e50e65f1ca789e33aec6e70159b25fd32f2
Malvin Ochieng <79262820+malvinopondo@users.noreply.github.com>
2026-08-01 21:00:25 +0300

    Add AuthController with login and logout routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
+
+Route::get('/jobs',         [ApiController::class, 'jobs']);
+Route::get('/applications', [ApiController::class, 'applications']);
+Route::get('/payments',     [ApiController::class, 'payments']);
+Route::get('/contacts',     [ApiController::class, 'contacts']);
+Route::get('/messages',     [ApiController::class, 'messages']);
+Route::get('/users',        [ApiController::class, 'users']);
