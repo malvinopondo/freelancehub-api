85b5ae4f04c434f7f721af97014daecc2941a932
Malvin Ochieng <79262820+malvinopondo@users.noreply.github.com>
2026-08-01 21:54:30 +0300

    Add registration route to API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ApiController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
